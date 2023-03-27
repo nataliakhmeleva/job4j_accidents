98467d8368f65befcd45718a51d4494a54a0ebde
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-03-27 12:44:54 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
