a720ab1e94b6220557a83f9a0052d1e9fbb7ff02
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-03-28 16:59:37 +0300

    4. Form с композиционным объектом [#305522 #404906]

--- src/main/java/ru/job4j/accidents/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentMem.java
@@ -5,27 +5,25 @@ import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.AccidentType;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class AccidentMem {
     private final AtomicInteger nextId = new AtomicInteger();
     private final Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
-    private final List<AccidentType> types = new CopyOnWriteArrayList<>(List.of(
-            new AccidentType(1, "Две машины"),
-            new AccidentType(2, "Машина и человек"),
-            new AccidentType(3, "Машина и велосипед")
+    private final Map<Integer, AccidentType> types = new ConcurrentHashMap<>(Map.of(
+            1, new AccidentType(1, "Две машины"),
+            2, new AccidentType(2, "Машина и человек"),
+            3, new AccidentType(3, "Машина и велосипед")
     ));
 
     public AccidentMem() {
-        save(new Accident(0, "Авария1", "ДТП с участием двух легковых автомобилей.", "г. Москва, пр-т Мира", types.get(0)));
-        save(new Accident(0, "Авария2", "ДТП с участием легкового и грузового автомобилей.", "г. Санкт-Петербург, Невский пр-т", types.get(0)));
-        save(new Accident(0, "Авария3", "ДТП с участием легкового автомобиля и пешехода.", "г. Сочи, ул. Пушкина", types.get(1)));
+        save(new Accident(0, "Авария1", "ДТП с участием двух легковых автомобилей.", "г. Москва, пр-т Мира", types.get(1)));
+        save(new Accident(0, "Авария2", "ДТП с участием легкового и грузового автомобилей.", "г. Санкт-Петербург, Невский пр-т", types.get(1)));
+        save(new Accident(0, "Авария3", "ДТП с участием легкового автомобиля и пешехода.", "г. Сочи, ул. Пушкина", types.get(2)));
     }
 
     public Accident save(Accident accident) {
@@ -47,10 +45,10 @@ public class AccidentMem {
     }
 
     public Optional<AccidentType> findByIdType(int id) {
-        return Optional.ofNullable(types.get(id - 1));
+        return Optional.ofNullable(types.get(id));
     }
 
     public Collection<AccidentType> findAllTypes() {
-        return types;
+        return types.values();
     }
 }
