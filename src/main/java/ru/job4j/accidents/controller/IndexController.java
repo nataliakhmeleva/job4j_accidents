f6743875fae3efcec818c97d54d8f85af94d88c9
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-04-03 11:14:11 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,19 +1,20 @@
 package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accidents.service.AccidentService;
 import ru.job4j.accidents.service.data.AccidentDataService;
 
 @Controller
 @AllArgsConstructor
 public class IndexController {
     private final AccidentDataService accidentService;
+
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("user", "Petr Arsentev");
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("accidents", accidentService.findAll());
         return "index";
     }
