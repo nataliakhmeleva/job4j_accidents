622f288388fbcd9a91509f356bf0167dc5ed7bd1
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-04-05 13:02:09 +0300

    2. Регистрация пользователя [#296069 #406668]

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -1,6 +1,8 @@
 package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,7 @@ import ru.job4j.accidents.service.data.UserDataService;
 @Controller
 @AllArgsConstructor
 public class RegController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegController.class.getName());
     private final PasswordEncoder encoder;
     private final UserDataService users;
     private final AuthorityDataService authorities;
@@ -23,14 +26,13 @@ public class RegController {
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        String errorMessage;
         try {
             users.save(user);
             return "redirect:/login";
         } catch (Exception e) {
-            errorMessage = "The user with login already exists.";
+            LOGGER.error("errorMessage", e);
         }
-        model.addAttribute("errorMessage", errorMessage);
+        model.addAttribute("errorMessage", "The user with login already exists.");
         return "reg";
     }
 
